cb4c8102281ddcf30f6eef4fd3bb134169903872
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-11-27 17:48:15 +0100

    Index.astro uit elkaar getrokken

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,3 +1,6 @@
+import type { MarkdownInstance } from "astro";
+import type { TegelFrontmatter } from "./types";
+
 /**
  * Difference between two dates in the number of years
  */
@@ -5,4 +8,27 @@ export const diffInYear = (a: Date, b: Date) => {
     let diffYear = (b.getTime() - a.getTime()) / 1000;
     diffYear /= 60 * 60 * 24;
     return Math.abs(Math.round(diffYear / 365.25));
+};
+
+/**
+ * Groepeer tegels by het jaartal
+ * @param arr Array van 'tegels'
+ * @returns Object van 'tegels' gegroepeerd op jaartal
+ */
+export const groupTegelsByJaar = <
+    T extends MarkdownInstance<TegelFrontmatter>,
+    K extends keyof T
+>(
+    arr: T[]
+): Record<string, T[]> => {
+    return arr.reduce(
+        (acc, item) => (
+            (acc[new Date(item.frontmatter.datum).getFullYear()] = [
+                ...(acc[new Date(item.frontmatter.datum).getFullYear()] || []),
+                item,
+            ]),
+            acc
+        ),
+        {} as Record<string, T[]>
+    );
 };
