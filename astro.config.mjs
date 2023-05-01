210b36bcce7a4d0d389989eba2db227959b51f85
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-05-01 12:29:47 +0200

    Add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://mhorrfilms.benjami.in",
+  site: "https://mhorrfilms.benjami.in",
+  integrations: [svelte()]
 });
