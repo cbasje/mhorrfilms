4429a0942082e381c8777f48873796ec23caa841
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-05-01 16:53:56 +0200

    Remove svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import mdx from "@astrojs/mdx";
-import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://mhorrfilms.benjami.in",
-    integrations: [svelte(), mdx()],
+    integrations: [mdx()],
 });
